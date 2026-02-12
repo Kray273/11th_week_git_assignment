a4ef1eec9aed7bc01216da83b5040bef6bdcac2a
kray273 <stoneofko@gmail.com>
2026-02-12 14:02:25 +0000

    A DT 모델링 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -1,3 +1,25 @@
+import warnings
+warnings.filterwarnings('ignore')
 import pandas as pd
 from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
 wine = load_wine()
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['target'] = wine.target
+X = df.drop('target', axis=1)
+y = df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+dt_clf = DecisionTreeClassifier(random_state=42)
+dt_params = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [2, 5],
+    'min_samples_split': [2, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+dt_grid = GridSearchCV(dt_clf, param_grid=dt_params, cv=5, scoring='accuracy', n_jobs=-1)
+dt_grid.fit(X_train, y_train)
+best_dt = dt_grid.best_estimator_
