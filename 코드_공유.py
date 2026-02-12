1d3fe711b69e45d8f9720010dcec6a62efc2d118
kray273 <stoneofko@gmail.com>
2026-02-12 14:02:26 +0000

    B XGB 모델링 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -1,3 +1,24 @@
+import warnings
+warnings.filterwarnings('ignore')
 import pandas as pd
 from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split, GridSearchCV
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+
 wine = load_wine()
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['target'] = wine.target
+X = df.drop('target', axis=1)
+y = df['target']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+xgb_clf = XGBClassifier(random_state=42, eval_metric='mlogloss')
+xgb_params = {
+    'learning_rate': [0.1, 0.01, 0.001],
+    'max_depth': [3, 5, 7, 9, 15],
+    'n_estimators': [50, 100, 200, 300]
+}
+xgb_grid = GridSearchCV(xgb_clf, param_grid=xgb_params, cv=5, scoring='accuracy', n_jobs=-1)
+xgb_grid.fit(X_train, y_train)
+best_xgb = xgb_grid.best_estimator_
